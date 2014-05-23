91bd82d4e53361708d69f1c16cd4fca62278b83b
kumar8600 <kumar1@hotmail.co.jp>
2014-05-24 07:22:49 +0900

    Synced from 80b69312622fd979d21d5ec8344204cd925bfc37

---
+++ app.js
@@ -0,0 +1,119 @@
+﻿// WebGLを使ったデモやるでｗ
+/// <reference path="Scripts/typings/threejs/three.d.ts" />
+var Demo = (function () {
+    function Demo(viewport) {
+        this.viewport = viewport;
+
+        //
+        // レンダラーの作成
+        //
+        this.renderer = new THREE.WebGLRenderer({ antialias: true });
+
+        // サイズ
+        var width = this.viewport.offsetWidth;
+        var height = this.viewport.offsetHeight;
+        this.renderer.setSize(this.viewport.offsetWidth, this.viewport.offsetHeight);
+
+        // 背景色
+        this.renderer.setClearColor(0x444444, 1);
+
+        // #viewportの子にレンダラーのDOMを追加
+        viewport.appendChild(this.renderer.domElement);
+
+        // クロックの開始
+        this.clock = new THREE.Clock(true);
+
+        //
+        // カメラの作成
+        //
+        var fov = 90;
+        var aspect = width / height;
+        this.camera = new THREE.PerspectiveCamera(fov, aspect);
+        this.camera.position = new THREE.Vector3(0, 0, 100);
+
+        //
+        // シーンの作成
+        //
+        this.scene = new THREE.Scene();
+
+        // 光源
+        var directionalLight = new THREE.DirectionalLight(0xffffff, 1.0);
+        directionalLight.position = new THREE.Vector3(0, 0, 1);
+        this.scene.add(directionalLight);
+
+        // ノイズで波打つシェーダーのマテリアルの作成
+        var vShader = document.getElementById('vshader').textContent;
+        var fShader = document.getElementById('fshader').textContent;
+        var uniforms = {
+            time: {
+                type: "f",
+                value: this.clock.getElapsedTime()
+            },
+            resolution: {
+                type: "v2",
+                value: new THREE.Vector2(width, height)
+            }
+        };
+        this.noiseMaterial = new THREE.ShaderMaterial({
+            vertexShader: vShader,
+            fragmentShader: fShader,
+            uniforms: uniforms,
+            wireframe: true
+        });
+
+        // 波打つメッシュ
+        var planeGeometry = new THREE.PlaneGeometry(2000, 2000, 100, 100);
+        var noisedMesh = new THREE.Mesh(planeGeometry, this.noiseMaterial);
+        noisedMesh.rotation.x = -Math.PI / 2.0;
+        noisedMesh.position.set(0, -100, 0);
+        this.scene.add(noisedMesh);
+        /*
+        // 既成のワイヤーフレームなマテリアルの作成
+        var wireframeMaterial = new THREE.MeshBasicMaterial({
+        color: 0x00ff00,
+        wireframe: true
+        });
+        
+        // タイトルのメッシュ
+        var titleGeometry = new THREE.TextGeometry("kumar8600", {
+        size: 80,
+        height: 20,
+        curveSegments: 2,
+        font: "helvetiker",
+        weight: "bold"
+        });
+        var titleMesh = new THREE.Mesh(titleGeometry, wireframeMaterial);
+        this.scene.add(titleMesh);
+        */
+    }
+    Demo.prototype.resize = function () {
+        this.renderer.setSize(this.viewport.offsetWidth, this.viewport.offsetHeight);
+    };
+
+    Demo.prototype.render = function () {
+        this.noiseMaterial.uniforms.time.value = this.clock.getElapsedTime() / 10.0;
+        this.renderer.render(this.scene, this.camera);
+    };
+    return Demo;
+})();
+
+window.onload = function () {
+    var demo = new Demo(document.getElementById("viewport"));
+
+    var loopRendering = function () {
+        // ブラウザに再描画の呼び出しを行わせる
+        requestAnimationFrame(loopRendering);
+
+        demo.render();
+    };
+
+    window.onresize = function () {
+        demo.resize();
+    };
+
+    loopRendering();
+};
+//
+//
+//
+//# sourceMappingURL=app.js.map
